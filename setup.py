55d15237e358e9a8f0880f66d1b244e9e64aa1c6
amiralihabibzadeh <b66669420@gmail.com>
2025-09-20 19:27:04 +0330

    Version Control

--- setup.py
+++ setup.py
@@ -2,22 +2,27 @@ from setuptools import setup, find_packages
 from pathlib import Path
 
 this_directory = Path(__file__).parent
-long_description = (this_directory / "README.md").read_text()
+long_description = (this_directory / "README.md").read_text(encoding="utf-8")
 
 setup(
     name="Qmorse",
     version="1.0.0",
     author="Hobab",
     author_email="b66669420@gmail.com",
-    description="A mini library for encoding and decoding Binary Morse code",
+    description="A lightweight Morse-like encoder/decoder library for Python",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/amiralihabibzadeh/Qmorse",
-    packages=find_packages(),
+    packages=find_packages(include=["qmorse", "qmorse.*"]),
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
+        "Topic :: Communications :: Morse Code",
+        "Topic :: Software Development :: Libraries :: Python Modules",
     ],
     python_requires=">=3.7",
+    install_requires=[],
+    include_package_data=True,
+    keywords="morse binary encoding decoding python",
 )
